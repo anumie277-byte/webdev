4bdce6a58e8b49b89d97c0876c2ccb29fc5c063a
anumie277-byte <anumie.277@gmail.com>
2026-05-09 14:57:55 +0500

    made the header same for mobile view

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const runawaybutton = document.getElementById("demo");
+const notouchybutton = document.querySelector(".homepagebutton");
+const notouchything = document.getElementById("demo");
+
+let audioPlaying = false;
 
 document.getElementById("demo").onmouseover = function () {
   mouseOver();
@@ -8,20 +11,22 @@ document.getElementById("demo").onmouseout = function () {
 };
 
 function mouseOver() {
-  document.getElementById("demo").innerHTML = "No touchy";
-  runawaybutton.style.left = Math.floor(Math.random() * 800) + "px";
-  runawaybutton.style.top = Math.floor(Math.random() * 800) + "px";
+  notouchything.innerHTML = "No touchy";
+  notouchyvid.play();
+  console.log("Playing audio");
+  audioPlaying = true;
+  notouchybutton.style.left = Math.floor(Math.random() * 800) + "px";
+  notouchybutton.style.top = Math.floor(Math.random() * 800) + "px";
 }
 
 function mouseOut() {
-  document.getElementById("demo").innerHTML = "Click me";
-}
-
-const notouchbutton = document.getElementById("demo");
-const notouchyvideo = document.getElementById("notouchyvid");
-
-notouchbutton.addEventListener("click", notouchsound);
-
-function notouchsound() {
-  notouchyvideo.play();
+  if (audioPlaying) {
+    notouchything.innerHTML = "No touchy";
+  } else {
+    notouchything.innerHTML = "Click me";
+  }
+  do {
+    notouchything.innerHTML = "No touchy";
+    audioPlaying = !notouchyvid.paused; // If it's NOT paused, it's playing
+  } while (audioPlaying);
 }
